0579cc7d39de1c68da23c79045a9d36641727b8e
brennaman <paulbrennaman@outlook.com>
2019-08-17 19:05:08 -0400

    update

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "region" {
 
 variable "prefix" {
   description = "This prefix will be included in the name of some resources."
-  default     = "mbdemo6"
+  default     = "mbdemo7"
 }
 
 variable "location" {
