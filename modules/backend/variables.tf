618b892f748efbb8057e55dc7bce96ff72dd31c7
brennaman <paulbrennaman@outlook.com>
2019-08-21 10:34:05 -0400

    update

--- modules/backend/variables.tf
+++ modules/backend/variables.tf
@@ -4,13 +4,13 @@ variable "location" {}
 
 #backend config specific to azure
 variable "resource_group_name" {
-  default="mb19820-terraform-state-group"
+  default="mb19821-terraform-state-group"
 }
 variable "storage_account_name" {
-  default="mb19820tfstatesa"
+  default="mb19821tfstatesa"
 }
 variable "container_name" {
-  default="mb19820-tfstate"
+  default="mb19821-tfstate"
 }
 variable "key" {
   default="terraform.state"
