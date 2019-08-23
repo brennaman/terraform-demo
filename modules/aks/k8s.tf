8220e79ff566414713a49b718f4adacfe7ed801e
brennaman <paulbrennaman@outlook.com>
2019-08-22 20:36:10 -0400

    update

--- modules/aks/k8s.tf
+++ modules/aks/k8s.tf
@@ -1,13 +1,13 @@
 provider "kubernetes" {
-    config_path   ="/root/.kube/config"
+    config_context   ="mb19821-aks-atfan-dev-eus-admin"
 }
 
-resource "null_resource" "cluster4" {
+resource "null_resource" "cluster5" {
 
     provisioner "local-exec" {
     command = <<EOT
     az login --service-principal --username ${var.AZURE_CLIENT_ID} --password ${var.AZURE_CLIENT_SECRET} --tenant ${var.AZURE_TENANT_ID}
-    az aks get-credentials --resource-group ${azurerm_resource_group.k8s.name} --name ${azurerm_kubernetes_cluster.k8s.name} -f "~/.kube/config" --admin
+    az aks get-credentials --resource-group ${azurerm_resource_group.k8s.name} --name ${azurerm_kubernetes_cluster.k8s.name} --admin
     kubectl get nodes
     ls -a
     cat ~/.kube/config
@@ -39,7 +39,7 @@ resource "kubernetes_cluster_role_binding" "kubernetes-dashboard-rule" {
     api_group = ""
   }
 
-  depends_on = [null_resource.cluster4]
+  depends_on = [null_resource.cluster5]
 
 }
 
@@ -55,7 +55,7 @@ resource "kubernetes_service_account" "tiller" {
     namespace = "kube-system"
   }
 
-  depends_on = [null_resource.cluster4]
+  depends_on = [null_resource.cluster5]
 
 }
 
@@ -81,6 +81,6 @@ resource "kubernetes_cluster_role_binding" "tiller-cluster-rule" {
     command = "helm init --service-account tiller"
   }
 
-  depends_on = [null_resource.cluster4]
+  depends_on = [null_resource.cluster5]
 
 }
